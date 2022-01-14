b1d603c3da16ccac209c06da6837f22fab2daf8a
Rafer <rafer.gluyas@gmail.com>
2022-01-14 11:25:54 +0800

    Update RunMe.java

--- RunMe.java
+++ RunMe.java
@@ -1,5 +1,7 @@
 public class RunMe {
   public static void main(String[] args){
     System.out.println("This is RunMe... running once");
+    System.out.println("Another line of text"
+                       );
   }
 }

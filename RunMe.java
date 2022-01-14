9d51012a6bee4cd4c3900dca919020d66a41d007
rafergDUG <97714534+rafergDUG@users.noreply.github.com>
2022-01-14 14:48:21 +0800

    Update RunMe.java
    more too long?

--- RunMe.java
+++ RunMe.java
@@ -4,7 +4,8 @@ public class RunMe {
    * Main method for saying hello to the world.
    */
   public static void main(final String[] args) {
-    System.out.println("Hello World - yes we've just got more information " +
+    System.out.println("Hello World - yes we've " +
+                       "just got more information " +
                        "after we say hello!");
   }
 }

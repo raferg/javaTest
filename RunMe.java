7ce8ac9259fc850dc2297e6a0edbee8d2f30d9c1
rafergDUG <97714534+rafergDUG@users.noreply.github.com>
2022-01-14 14:50:16 +0800

    Update RunMe.java
    + newline

--- RunMe.java
+++ RunMe.java
@@ -4,8 +4,8 @@ public class RunMe {
    * Main method for saying hello to the world.
    */
   public static void main(final String[] args) {
-    System.out.println("Hello World - yes we've " +
-                       "just got more information " +
-                       "after we say hello!");
+    System.out.println("Hello World - yes we've "
+                       + "just got more information "
+                       + "after we say hello!");
   }
 }

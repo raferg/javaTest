ae32ebec50eda3b6ae2ee62ca714d330f2c0844b
rafergDUG <97714534+rafergDUG@users.noreply.github.com>
2022-01-14 14:46:21 +0800

    Update RunMe.java
    line length

--- RunMe.java
+++ RunMe.java
@@ -4,6 +4,7 @@ public class RunMe {
    * Main method for saying hello to the world.
    */
   public static void main(final String[] args) {
-    System.out.println("Hello World - yes we've just got more information after we say hello!");
+    System.out.println("Hello World - yes we've just got more information " +
+                       "after we say hello!");
   }
 }

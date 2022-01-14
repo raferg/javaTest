c190913e5a25d373a05665da7c43cf9584b8c572
Rafer <rafer.gluyas@gmail.com>
2022-01-14 14:13:37 +0800

    Update RunMe.java

--- RunMe.java
+++ RunMe.java
@@ -1,7 +1,9 @@
 public class RunMe {
-  public static void main(String[] args){
-    System.out.println(
-      "Hello World"
-    );
+
+  /**
+   * Main method for saying hello to the world.
+   */
+  public static void main(@param final String[] args) {
+    System.out.println("Hello World");
   }
 }

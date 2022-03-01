bd3ddf5a71305290ebacb75713ad57548e027cfc
Rafer <rafer.gluyas@dst.defence.gov.au>
2022-03-01 09:34:50 +0800

    TAB testing

--- src/RunMe.java
+++ src/RunMe.java
@@ -1,9 +1,13 @@
 public class RunMe {
-
-  /**
+	
+  /**	
    * Main method for saying hello to the world.  
    */  
-  public static void main(final String[] args) {  
-    System.out.println("Hello World");  
+  public static void main(final String[] args) {
+    System.out.println("Hello World");
   }
+
+  //TAB at the end	
+  //TAB in the (	) middle
+  //Just spaces at the end  
 }

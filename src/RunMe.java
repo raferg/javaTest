4ac52828e426097916060a8c6d4d554292d964f3
Rafer <rafer.gluyas@dst.defence.gov.au>
2022-03-01 11:37:44 +0800

    various spaces and whitespace and tab

--- src/RunMe.java
+++ src/RunMe.java
@@ -6,4 +6,9 @@ public class RunMe {
   public static void main(final String[] args) {  
     System.out.println("Hello World");  
   }
+
+	//Leading tab
+  //Trailing tab	
+  //Nothing rule breaking
+  //Trailing space  
 }

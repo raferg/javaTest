84b7aa481480e01942e169bcecb283f1d8846e19
David Adams <dbadams@iinet.net.au>
2022-03-03 12:12:08 +0800

    Revert "some changes"
    This reverts commit 55828ec38a798c737546c3dc9f3ed59063ff4901.

--- src/RunMe.java
+++ src/RunMe.java
@@ -4,7 +4,6 @@ public class RunMe {
    * Main method for saying hello to the world.  
    */  
   public static void main(final String[] args) {  
-    System.out.println("Hello World");
-    System.out.println("This is a test");
+    System.out.println("Hello World");  
   }
 }

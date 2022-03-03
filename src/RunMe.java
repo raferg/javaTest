55828ec38a798c737546c3dc9f3ed59063ff4901
David Adams <dbadams@iinet.net.au>
2022-03-03 12:11:08 +0800

    some changes

--- src/RunMe.java
+++ src/RunMe.java
@@ -4,6 +4,7 @@ public class RunMe {
    * Main method for saying hello to the world.  
    */  
   public static void main(final String[] args) {  
-    System.out.println("Hello World");  
+    System.out.println("Hello World");
+    System.out.println("This is a test");
   }
 }
--- src/example.java
+++ src/example.java
@@ -2,7 +2,7 @@
 public class example
 {
   public static void main(String[] args) {
-
+    System.out.println("this is a test");
     return;
   }
 }
